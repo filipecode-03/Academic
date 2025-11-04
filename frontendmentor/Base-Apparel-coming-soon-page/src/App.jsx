bb92ad1973dd188e9cf93aead8cee91bf97f832f
Filipe Magalhães <contatofilipecode@gmail.com>
2025-11-04 17:00:16 -0400

    update

--- frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
+++ frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import logo from './assets/images/logo.svg'
 import pictureMobile from './assets/images/hero-mobile.jpg'
+import Email from './assets/components/Email.jsx'
 
 function App() {
 
   return (
-    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-pink-200 to-pink-500 min-h-screen py-[50px] w-full text-center">
+    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-white to-[#fff5f5] pb-[70px] min-h-screen w-full text-center">
       <img src={logo} alt="logo" className='p-[50px]' />
       <div>
         <img src={pictureMobile} alt="picture" className='w-full' />
-        <div className='pt-[80px] px-[30px] space-y-[15px]'>
-          <h1 className='text-[57px] leading-[60px] tracking-[10px]'><span className='font-light'>WE'RE</span> COMING SOON</h1>
-          <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+        <div className='pt-20 px-[30px] space-y-[15px]'>
+          <h1 className='text-[57px] leading-[60px] tracking-[10px] text-[#423e3d]'><span className='font-light text-[#C19799]'>WE'RE</span> COMING SOON</h1>
+          <p className='text-[#C19799] text-[18px] px-5'>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+          <Email />
         </div>
       </div>
     </div>
--- frontendmentor/Base-Apparel-coming-soon-page/src/assets/components/Email.jsx
+++ frontendmentor/Base-Apparel-coming-soon-page/src/assets/components/Email.jsx
@@ -1,1 +1,15 @@
+import arrow from '../images/icon-arrow.svg'
 
+function Email() {
+    return (
+        <div>
+            <input type="email" name="" id="" />
+            <button>
+                <img src={arrow} alt="" />
+            </button>
+        </div>
+    )
+
+}
+
+export default Email
