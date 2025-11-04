326eed7d50cbb1ce36819d19f7cde2e2b9b89d90
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2025-11-03 20:03:12 -0400

    update

--- frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
+++ frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
@@ -5,14 +5,13 @@ import pictureMobile from './assets/images/hero-mobile.jpg'
 function App() {
 
   return (
-    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-white to-[#fff5f5] h-screen w-full text-center">
+    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-pink-200 to-pink-500 min-h-screen py-[50px] w-full text-center">
       <img src={logo} alt="logo" className='p-[50px]' />
       <div>
         <img src={pictureMobile} alt="picture" className='w-full' />
-        <div className=''>
-          <h1 className='text-[50px] '><span className='font-light'>WE'RE</span> COMING SOON</h1>
+        <div className='pt-[80px] px-[30px] space-y-[15px]'>
+          <h1 className='text-[57px] leading-[60px] tracking-[10px]'><span className='font-light'>WE'RE</span> COMING SOON</h1>
           <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
-          <label htmlFor="email"></label>
         </div>
       </div>
     </div>
