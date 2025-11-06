cba4dc01009e465173d5871ba4d90dfd8a71e398
Filipe Magalhães <contatofilipecode@gmail.com>
2025-11-06 10:09:31 -0400

    finished the project article-preview-component

--- frontendmentor/article-preview-component/src/App.jsx
+++ frontendmentor/article-preview-component/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
         <img src={picture} alt="picture" className='rounded-t-2xl 
                                                     lg:rounded-l-2xl lg:rounded-r-none lg:w-[450px]' />
         <div className='space-y-4'>
-          <h1 className='px-10 pt-10 font-bold text-[#48556A] text-[21px]'>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+          <h1 className='px-10 pt-10 font-bold text-[#48556A] text-[21px]
+                        lg:text-[23px]'>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
           <p className='px-10 text-[#48556A] tracking-[1px]'>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
           <Profile2 />
         </div>
