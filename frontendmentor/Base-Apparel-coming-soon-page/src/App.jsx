fa3158f4b05facf9598b90638258d0072cf8e0ba
Filipe Magalhães <contatofilipecode@gmail.com>
2025-11-07 16:27:39 -0400

    update

--- frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
+++ frontendmentor/Base-Apparel-coming-soon-page/src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from 'react'
 import logo from './assets/images/logo.svg'
 import pictureMobile from './assets/images/hero-mobile.jpg'
 import Email from './assets/components/Email.jsx'
+import pictureDesktop from './assets/images/hero-desktop.jpg'
 
 function App() {
 
   return (
-    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-white to-[#fff5f5] pb-[70px] min-h-screen w-full text-center">
-      <img src={logo} alt="logo" className='p-[50px]' />
+    <div className="font-['Josefin_Sans'] bg-gradient-to-b from-white to-[#fff5f5] pb-[70px] min-h-screen w-full text-center
+                    lg:flex lg:pb-0">
+      <img src={logo} alt="logo" className='p-[50px] block lg:hidden' />
       <div>
-        <img src={pictureMobile} alt="picture" className='w-full' />
+        <img src={pictureMobile} alt="picture" className='w-full block lg:hidden' />
+        <img src={logo} alt="logo" className='pl-[100px] pt-[60px] hidden lg:block' />
         <div className='pt-20 px-[30px] space-y-[15px]'>
           <h1 className='text-[57px] leading-[60px] tracking-[10px] text-[#423e3d]'><span className='font-light text-[#C19799]'>WE'RE</span> COMING SOON</h1>
-          <p className='text-[#C19799] text-[18px] px-5'>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+          <p className='text-[#C19799] text-[18px] px-6'>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
           <Email />
         </div>
       </div>
+      <div><img src={pictureDesktop} alt="picture" class="hidden lg:block h-full w-[750px]"></img></div>
     </div>
   )
 }
--- frontendmentor/Base-Apparel-coming-soon-page/src/assets/components/Email.jsx
+++ frontendmentor/Base-Apparel-coming-soon-page/src/assets/components/Email.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react'
 import arrow from '../images/icon-arrow.svg'
+import alert from '../images/icon-error.svg'
+import '../css/App.css'
 
 function Email() {
+
+    const [email, setEmail] = useState(false)
+
     return (
-        <div>
-            <input type="email" name="" id="" />
-            <button>
-                <img src={arrow} alt="" />
-            </button>
+        <div className='pt-5'>
+            <form 
+                action="/enviar" 
+                method="post" 
+                class={`flex items-center bg-[#fff5f5] rounded-full overflow-hidden border w-[85%] mx-auto ${email ? 'border-red-500' : 'border-[#C19799]'}`}>
+                <label for="email" class="sr-only">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                    required
+                    class="flex-grow px-7 py-4 text-[18px] text-[#c19799] placeholder-[#c19799] bg-transparent focus:outline-none"
+                />
+                {email && (
+                    <img src={alert} alt="icon error" className='pr-4' />
+                )}
+                <button 
+                    onClick={() => setEmail(!email)}
+                    type="submit" 
+                    class="bg-gradient-to-r from-[#f8bfbf] to-[#ee8c8c] py-[20px] px-[35px] rounded-full flex items-center justify-center shadowb hover:scale-110"
+                >
+                    <img src={arrow} alt="icon arrow" className='h-[28px]' />
+                </button>
+            </form>
+            {email && (
+                <p className='text-left text-red-500 pl-[64px] pt-3'>Please provide a valid email</p>
+            )}
         </div>
     )
 
