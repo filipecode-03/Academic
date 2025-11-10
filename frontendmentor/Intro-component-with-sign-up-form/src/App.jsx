3e6cd01e48387e68629d8341d8ef890955baedf3
Filipe Magalhães <contatofilipecode@gmail.com>
2025-11-10 16:48:48 -0400

    update

--- frontendmentor/Intro-component-with-sign-up-form/src/App.jsx
+++ frontendmentor/Intro-component-with-sign-up-form/src/App.jsx
@@ -1,10 +1,13 @@
-
+import "./App.css"
 
 function App() {
   
   return (
-    <div>
-      
+    <div className="bg-red-400/80 min-h-screen font-['Poppins']">
+      <div className="bgroud text-center text-white">
+        <h1 className="font-bold text-[40px] px-[60px] pt-[100px] leading-[50px]">Learn to code by watching others</h1>
+        <p className="px-[60px] pt-[30px] text-[20px]">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
+      </div>
     </div>
   )
 }
