9013f26e6ccf2320ebc391a86b2e049f97fa97a8
Filipe Magalhães <158044004+filipecode-03@users.noreply.github.com>
2026-01-14 20:53:42 +0000

    update

--- Frontendmentor/Agency-landing-page/src/assets/components/Sec01.jsx
+++ Frontendmentor/Agency-landing-page/src/assets/components/Sec01.jsx
@@ -3,14 +3,14 @@ import arrow from '../images/icon-arrow-down.svg'
 
 function Sec01() {
     return (
-        <div className="text-center text-white bg-[url('./assets/images/desktop/image-header.jpg')] bg-cover bg-center h-screen p-10">
+        <div className="hidden lg:block text-center text-white bg-[url('./assets/images/desktop/image-header.jpg')] bg-cover bg-center h-screen p-10">
             <div className='flex items-start justify-between'>
                 <img src={logo} alt="logo" className='w-50 mt-2' />
                 <div className='flex gap-12 text-[18px] font-medium'>
-                    <p className='my-auto'>About</p>
-                    <p className='my-auto'>Services</p>
-                    <p className='my-auto'>Projects</p>
-                    <p className='bg-white rounded-full p-3 px-6 text-black font-["Fraunces"] font-bold'>CONTACT</p>
+                    <p className='my-auto cursor-pointer'>About</p>
+                    <p className='my-auto cursor-pointer'>Services</p>
+                    <p className='my-auto cursor-pointer'>Projects</p>
+                    <p className='bg-white cursor-pointer hover:bg-[#ffffff5d] hover:text-white rounded-full p-3 px-6 text-black font-["Fraunces"] font-bold'>CONTACT</p>
                 </div>
             </div>
             <h1 className='font-["Fraunces"] font-extrabold mt-17 text-[70px] tracking-[8px]'>WE ARE CREATIVES</h1>
