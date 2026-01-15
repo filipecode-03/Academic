252ad734e90b816dc925af3ce4dfc2c375816e60
Filipe Magalhães <158044004+filipecode-03@users.noreply.github.com>
2026-01-15 14:41:24 +0000

    finished the project

--- Frontendmentor/Agency-landing-page/src/App.jsx
+++ Frontendmentor/Agency-landing-page/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import eggMobile from './assets/images/mobile/image-transform.jpg'
+import eggDesktop from './assets/images/desktop/image-transform.jpg'
 import logo from './assets/images/logo.svg'
 import logoGreen from './assets/images/logo-green.png'
 import menuHamburguer from './assets/images/icon-hamburger.svg'
 import arrow from './assets/images/icon-arrow-down.svg'
 import juiceMobile from './assets/images/mobile/image-stand-out.jpg'
+import juiceDesktop from './assets/images/desktop/image-stand-out.jpg'
 import emily from './assets/images/image-emily.jpg'
 import thomas from './assets/images/image-thomas.jpg'
 import jennie from './assets/images/image-jennie.jpg'
 import milkMobile from './assets/images/mobile/image-gallery-milkbottles.jpg'
+import milkDesktop from './assets/images/desktop/image-gallery-milkbottles.jpg'
 import orangeMobile from './assets/images/mobile/image-gallery-orange.jpg'
+import orangeDesktop from './assets/images/desktop/image-gallery-orange.jpg'
 import coneMobile from './assets/images/mobile/image-gallery-cone.jpg'
+import coneDesktop from './assets/images/desktop/image-gallery-cone.jpg'
 import sugarMobile from './assets/images/mobile/image-gallery-sugar-cubes.jpg'
+import sugarDesktop from './assets/images/desktop/image-gallery-sugarcubes.jpg'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faInstagram, faSquareFacebook, faPinterest } from '@fortawesome/free-brands-svg-icons'
 import Sec01 from './assets/components/Sec01'
+import Sec02 from './assets/components/Sec02'
 
 function App() {
 
@@ -45,29 +52,31 @@ function App() {
 
       <Sec01 />
       
-      <div className='text-center'>
-        <img src={eggMobile} alt="egg" />
-        <div className='p-8 space-y-8 pt-16 pb-14'>
-          <h1 className="font-['Fraunces'] font-bold text-[35px] w-[80%] mx-auto leading-10">Transform your brand</h1>
-          <p className="font-medium text-gray-700 text-[18px]">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+      <div className='text-center lg:text-left lg:flex lg:flex-row-reverse'>
+        <img src={eggMobile} alt="egg" className='block lg:hidden' />
+        <img src={eggDesktop} alt="egg" className='hidden lg:block lg:w-530' />
+        <div className='p-8 lg:p-30 space-y-8 pt-16 lg:pt-40 pb-14'>
+          <h1 className="font-['Fraunces'] font-bold text-[35px] lg:text-[55px] w-[80%] lg:w-full mx-auto leading-10 lg:leading-15">Transform your brand</h1>
+          <p className="font-medium text-gray-700 text-[18px] lg:text-[20px]">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
           <p className="group relative inline-block w-fit font-extrabold font-['Fraunces'] tracking-[1px] cursor-pointer">
           LEARN MORE
           <span className="absolute -left-2.5 bottom-0.5 h-2 w-[115%] bg-[#fff2ab] rounded-[10px] -z-10 transition-colors duration-300 group-hover:bg-[#FFD100]"></span>
           </p>
         </div>
       </div>
-      <div className='text-center'>
-        <img src={juiceMobile} alt="juice" />
-        <div className='p-8 space-y-8 pt-16 pb-14'>
-          <h1 className="font-['Fraunces'] font-bold text-[35px] w-[80%] mx-auto leading-10">Stand out to the right audience</h1>
-          <p className="font-medium text-gray-700 text-[18px]">Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.</p>
+      <div className='text-center lg:text-left lg:flex'>
+        <img src={juiceMobile} alt="juice" className='block lg:hidden' />
+        <img src={juiceDesktop} alt="juice" className='hidden lg:block lg:w-478' />
+        <div className='p-8 lg:p-30 space-y-8 pt-16 lg:pt-38 pb-14'>
+          <h1 className="font-['Fraunces'] font-bold text-[35px] lg:text-[55px] w-[80%] lg:w-full mx-auto leading-10 lg:leading-15">Stand out to the right audience</h1>
+          <p className="font-medium text-gray-700 text-[18px] lg:text-[20px]">Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.</p>
           <p className="group relative inline-block w-fit font-extrabold font-['Fraunces'] tracking-[1px] cursor-pointer">
           LEARN MORE
           <span className="absolute -left-2.5 bottom-0.5 h-2 w-[115%] bg-[#FED9D0] rounded-[10px] -z-10 transition-colors duration-300 group-hover:bg-[#FD7863]"></span>
           </p>
         </div>
       </div>
-      <div>
+      <div className='block lg:hidden'>
         <div className="text-[#1A5245] pt-140 px-8 space-y-5 text-center bg-[url('./assets/images/mobile/image-graphic-design.jpg')] bg-cover bg-center h-200">
           <h1 className="font-['Fraunces'] text-[38px] font-extrabold">Graphic Design</h1>
           <p className='text-[18px] font-medium'>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
@@ -77,29 +86,32 @@ function App() {
           <p className='text-[18px] font-medium'>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
         </div>
       </div>
-      <div className='text-center p-8 space-y-15 pt-16 pb-14'>
+
+      <Sec02 />
+
+      <div className='text-center p-8 space-y-15 pt-16 lg:pt-36 pb-14'>
         <h1 className="font-['Fraunces'] font-bold tracking-[5px] text-gray-400 text-[20px]">CLIENT TESTIMONIALS</h1>
-        <div className='space-y-15'>
+        <div className='space-y-15 lg:flex lg:px-20'>
           <div>
-            <div className='space-y-8'>
+            <div className='space-y-8 lg:space-y-15'>
               <img src={emily} alt="emily" className='mx-auto rounded-full w-20' />
-              <p className='text-[18px]'>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
+              <p className='text-[18px] lg:w-[80%] lg:mx-auto'>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
               <h1 className="font-['Fraunces'] font-bold text-[20px]">Emily R.</h1>
             </div>
             <h2 className='text-gray-500'>Marketing Director</h2>
           </div>
           <div>
-            <div className='space-y-8'>
+            <div className='space-y-8 lg:space-y-15'>
               <img src={thomas} alt="emily" className='mx-auto rounded-full w-20' />
-              <p className='text-[18px]'>Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</p>
+              <p className='text-[18px] lg:w-[80%] lg:mx-auto'>Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</p>
               <h1 className="font-['Fraunces'] font-bold text-[20px]">Thomas S.</h1>
             </div>
             <h2 className='text-gray-500'>Chief Operating Officer</h2>
           </div>
           <div>
-            <div className='space-y-8'>
+            <div className='space-y-8 lg:space-y-15'>
               <img src={jennie} alt="emily" className='mx-auto rounded-full w-20' />
-              <p className='text-[18px]'>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
+              <p className='text-[18px] lg:w-[80%] lg:mx-auto'>Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!</p>
               <h1 className="font-['Fraunces'] font-bold text-[20px]">Jennie F.</h1>
             </div>
             <h2 className='text-gray-500'>Business Owner</h2>
@@ -108,20 +120,24 @@ function App() {
       </div>
       <section>
         <div className="grid grid-cols-2 md:grid-cols-4">
-          <img src={milkMobile} alt="milk" className="w-full h-auto object-cover" />
-          <img src={orangeMobile} alt="orange" className="w-full h-auto object-cover" />
-          <img src={coneMobile} alt="cone" className="w-full h-auto object-cover" />
-          <img src={sugarMobile} alt="sugar" className="w-full h-auto object-cover" />
+          <img src={milkMobile} alt="milk" className="block lg:hidden w-full h-auto object-cover" />
+          <img src={milkDesktop} alt="milk" className="hidden lg:block w-full h-auto object-cover" />
+          <img src={orangeMobile} alt="orange" className="block lg:hidden w-full h-auto object-cover" />
+          <img src={orangeDesktop} alt="orange" className="hidden lg:block w-full h-auto object-cover" />
+          <img src={coneMobile} alt="cone" className="block lg:hidden w-full h-auto object-cover" />
+          <img src={coneDesktop} alt="cone" className="hidden lg:block w-full h-auto object-cover" />          
+          <img src={sugarMobile} alt="sugar" className="block lg:hidden w-full h-auto object-cover" />
+          <img src={sugarDesktop} alt="sugar" className="hidden lg:block w-full h-auto object-cover" />
         </div>
       </section>
       <footer className='bg-[#90D4C5] pt-18 pb-20 px-10'>
         <img src={logoGreen} alt="logo green" className='mx-auto' />
-        <div className='flex mt-12 justify-between text-[18px] text-[#266758] w-[80%] mx-auto'>
+        <div className='flex mt-12 justify-between text-[18px] text-[#266758] w-[80%] lg:w-[25%] mx-auto'>
           <p className='hover:text-white cursor-pointer'>About</p>
           <p className='hover:text-white cursor-pointer'>Services</p>
           <p className='hover:text-white cursor-pointer'>Projects</p>
         </div>
-        <div className='mt-22 flex justify-between w-[50%] mx-auto'>
+        <div className='mt-22 flex justify-between w-[50%] lg:w-[15%] mx-auto'>
           <FontAwesomeIcon icon={faSquareFacebook} className="text-[#266758] hover:text-white cursor-pointer text-2xl" />
           <FontAwesomeIcon icon={faInstagram} className="text-[#266758] hover:text-white cursor-pointer text-2xl" />
           <FontAwesomeIcon icon={faTwitter} className="text-[#266758] hover:text-white cursor-pointer text-2xl" />
---
+++ Frontendmentor/Agency-landing-page/src/assets/components/Sec02.jsx
@@ -0,0 +1,18 @@
+function Sec02() {
+    return (
+        <div className="hidden lg:block">
+            <div className="flex">
+                <div className="text-[#1A5245] pt-135 px-8 space-y-5 text-center w-378 bg-[url('./assets/images/desktop/image-graphic-design.jpg')] bg-cover bg-center h-200">
+                    <h1 className="font-['Fraunces'] text-[48px] font-extrabold">Graphic Design</h1>
+                    <p className='text-[20px] leading-8 w-[60%] mx-auto font-medium'>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.</p>
+                </div>
+                <div className="text-[#1F546E] pt-135 px-10 space-y-5 text-center w-full bg-[url('./assets/images/desktop/image-photography.jpg')] bg-cover bg-center h-200">
+                    <h1 className="font-['Fraunces'] text-[48px] font-extrabold">Photography</h1>
+                    <p className='text-[20px] leading-8 font-medium w-[60%] mx-auto'>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sec02
