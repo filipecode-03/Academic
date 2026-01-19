b33f5354f5b75bce053839babfbef8fda03cc667
Filipe Magalhães <contatofilipecode@gmail.com>
2026-01-19 14:56:43 -0400

    update

--- Frontendmentor/loopstudios-landing-page/src/App.jsx
+++ Frontendmentor/loopstudios-landing-page/src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import logo from './assets/images/logo.svg'
+import menu from './assets/images/icon-hamburger.svg'
 
 function App() {
 
   return (
     <div className='min-h-screen'>
-      
+      <section className='p-8 pt-14 bg-[url("./assets/images/mobile/image-hero.jpg")] bg-cover h-screen'>
+        <div className='flex justify-between items-center'>
+          <img src={logo} alt="logo" />
+          <img src={menu} alt="menu" />
+        </div>
+        <h1 className='border text-white border-white'>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
+      </section>
     </div>
   )
 }
