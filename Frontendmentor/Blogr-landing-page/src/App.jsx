1dff67ba7a6fb9c33e124cb82ba6ad1d35236379
Filipe Magalhães <contatofilipecode@gmail.com>
2026-01-21 13:28:40 -0400

    finished the project

--- Frontendmentor/Blogr-landing-page/src/App.jsx
+++ Frontendmentor/Blogr-landing-page/src/App.jsx
@@ -4,8 +4,10 @@ import logo from './assets/images/logo.svg'
 import hamburguer from './assets/images/icon-hamburger.svg'
 import close from './assets/images/icon-close.svg'
 import editorMobile from './assets/images/illustration-editor-mobile.svg'
+import editorDesktop from './assets/images/group.png'
 import phones from './assets/images/illustration-phones.svg'
 import laptopMobile from './assets/images/illustration-laptop-mobile.svg'
+import laptopDesktop from './assets/images/group2.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { useFloating, offset, flip, shift, autoUpdate, } from "@floating-ui/react";
@@ -167,7 +169,7 @@ function App() {
           )}
           <div className='mt-20 lg:mt-30 space-y-5 lg:space-y-10'>
             <h1 className='font-["Ubuntu"] w-[80%] mx-auto leading-12 text-[38px] lg:text-[58px]'>A modern publishing platform</h1>
-            <p className='font-light text-[18px] w-[70%] mx-auto'>Grow your audience and build your online brand</p>
+            <p className='font-light text-[18px] lg:text-[20px] w-[70%] mx-auto'>Grow your audience and build your online brand</p>
           </div>
           <div className='mt-15 space-x-5 font-["Ubuntu"] font-bold'>
             <button className='bg-white text-[#FF4B5A] p-3 px-5 rounded-full hover:bg-[#ffffff48] transition-all duration-300 cursor-pointer hover:text-white'>Start for Free</button>
@@ -175,69 +177,73 @@ function App() {
           </div>
         </div>
       </nav>
-      <section className='text-center mt-25'>
-        <h1 className='font-["Ubuntu"] text-[28px]'>Designed for the future</h1>
-        <div className='mt-10 space-y-10'>
-          <img src={editorMobile} alt="editor" className='mx-auto' />
-          <div className='px-12 space-y-4'>
-            <h1 className='font-["Ubuntu"] text-[28px] px-15 leading-8'>Introducing an extensible editor</h1>
-            <p className='text-gray-600'>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
-          </div>
-          <div className='px-12 space-y-4'>
-            <h1 className='font-["Ubuntu"] text-[28px] px-15 leading-8'>Robust content management</h1>
-            <p className='text-gray-600'>Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+      <section className='text-center lg:text-left mt-25'>
+        <h1 className='font-["Ubuntu"] lg:text-center text-[28px] lg:text-[38px]'>Designed for the future</h1>
+        <div className='mt-10 space-y-10 lg:flex lg:flex-row-reverse'>
+          <img src={editorMobile} alt="editor" className='mx-auto block lg:hidden' />
+          <img src={editorDesktop} alt="editor" className='hidden lg:block lg:absolute lg:top-170 lg:z-50' />
+          <div className='lg:px-34 space-y-10 lg:pr-180 lg:space-y-15 lg:mt-20'>
+            <div className='px-12 space-y-4 lg:space-y-6'>
+              <h1 className='font-["Ubuntu"] text-[28px] px-15 lg:p-0 leading-8'>Introducing an extensible editor</h1>
+              <p className='text-gray-600'>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
+            </div>
+            <div className='px-12 space-y-4 lg:space-y-6'>
+              <h1 className='font-["Ubuntu"] text-[28px] px-15 lg:p-0 leading-8'>Robust content management</h1>
+              <p className='text-gray-600'>Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+            </div>
           </div>
         </div>
       </section>
 
-      <img src={phones} alt="phones" className='absolute z-11 top-450' />
+      <img src={phones} alt="phones" className='absolute z-11 top-450 lg:top-393 lg:left-30' />
       
-      <section className='mt-80 text-center text-white rounded-bl-[150px] hero rounded-tr-[150px]'>
-        <div className='pt-70 pb-40 px-15 space-y-7'>
+      <section className='mt-80 lg:mt-95 text-center lg:text-left text-white rounded-bl-[150px] hero rounded-tr-[150px]'>
+        <div className='pt-80 lg:pt-28 pb-40 lg:pb-30 px-15 lg:pl-180 lg:pr-45 space-y-7'>
           <h1 className='font-["Ubuntu"] text-[44px] leading-14'>State of the Art Infrastructure</h1>
           <p className='text-[18px]'>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
         </div>
       </section>
-      <section className='text-center mt-20'>
-        <img src={laptopMobile} alt="laptop" />
-        <div className='px-12 space-y-10 mt-10'>
+      <section className='text-center lg:text-left mt-20 lg:mt-40 lg:flex'>
+        <img src={laptopMobile} alt="laptop" className='block lg:hidden' />
+        <img src={laptopDesktop} alt="laptop" className='hidden lg:block' />
+        <div className='px-12 lg:px-22 lg:pr-50 space-y-10 lg:space-y-20 mt-10 lg:mt-50'>
           <div className='space-y-6'>
-            <h1 className='font-["Ubuntu"] text-[28px] px-15 leading-8'>Free, open, simple</h1>
+            <h1 className='font-["Ubuntu"] text-[28px] px-15 lg:px-0 leading-8'>Free, open, simple</h1>
             <p className='text-gray-600'>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
           </div>
           <div className='space-y-6'>
-            <h1 className='font-["Ubuntu"] text-[28px] px-15 leading-8'>Powerful tooling</h1>
+            <h1 className='font-["Ubuntu"] text-[28px] px-15 lg:px-0 leading-8'>Powerful tooling</h1>
             <p className='text-gray-600'>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
           </div>
         </div>
       </section>
-      <footer className='text-center mt-20 bg-[#24242C] rounded-tr-[150px] py-20 text-[18px]'>
-        <img src={logo} alt="logo" className='mx-auto' />
-        <div className='space-y-5 mt-15'>
+      <footer className='lg:flex lg:items-start text-center lg:text-left mt-20 bg-[#24242C] rounded-tr-[150px] py-20 text-[18px]'>
+        <img src={logo} alt="logo" className='mx-auto lg:mx-40' />
+        <div className='space-y-5 mt-15 lg:mt-0'>
           <h1 className='text-white font-["Ubuntu"] font-medium'>Product</h1>
           <div className='space-y-1 text-gray-300'>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Overview</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Pricing</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Marketplace</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Features</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Integrations</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Overview</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Pricing</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Marketplace</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Features</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Integrations</p>
           </div>
         </div>
-        <div className='space-y-5 mt-15'>
+        <div className='space-y-5 mt-15 lg:mt-0 lg:ml-60'>
           <h1 className='text-white font-["Ubuntu"] font-medium'>Company</h1>
           <div className='space-y-1 text-gray-300'>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">About</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Team</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Blog</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Careers</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">About</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Team</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Blog</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Careers</p>
           </div>
         </div>
-        <div className='space-y-5 mt-15'>
+        <div className='space-y-5 mt-15 lg:mt-0 lg:ml-60'>
           <h1 className='text-white font-["Ubuntu"] font-medium'>Connect</h1>
           <div className='space-y-1 text-gray-300'>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Contact</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">Newsletter</p>
-            <p className="hover:underline cursor-pointer w-fit mx-auto">LinkedIn</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Contact</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">Newsletter</p>
+            <p className="hover:underline cursor-pointer w-fit mx-auto lg:mx-0">LinkedIn</p>
           </div>
         </div>
       </footer>
