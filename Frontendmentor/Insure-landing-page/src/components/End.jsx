8f1425d0c9d4c847f09f493aecf21d433f0efc22
filipecode-03 <contatofilipecode@gmail.com>
2026-01-30 21:41:12 -0400

    finished the project

--- Frontendmentor/Insure-landing-page/src/components/End.jsx
+++ Frontendmentor/Insure-landing-page/src/components/End.jsx
@@ -4,40 +4,52 @@ import { faTwitter, faInstagram, faSquareFacebook, faPinterest } from '@fortawes
 
 function End() {
     return (
-        <footer className='tracking-[1px] py-35 text-center p-8 bg-[#FAFAFA] bg-[url("./assets/images/bg-pattern-footer-mobile.svg")] bg-no-repeat bg-top bg-size-[100%_auto]'>
-            <img src={logo} alt="logo" className='mx-auto' /> 
-            <div className='flex items-center justify-center text-gray-600 gap-2 mt-8 text-[28px]'>
-                <FontAwesomeIcon icon={faSquareFacebook} className='hover:text-black cursor-pointer' />
-                <FontAwesomeIcon icon={faTwitter} className='hover:text-black cursor-pointer' />
-                <FontAwesomeIcon icon={faPinterest} className='hover:text-black cursor-pointer' />
-                <FontAwesomeIcon icon={faInstagram} className='hover:text-black cursor-pointer' />
+        <footer className='tracking-[1px] py-35 lg:py-15 text-center lg:text-left p-8 lg:px-30 bg-[#FAFAFA] bg-[url("./assets/images/bg-pattern-footer-mobile.svg")] lg:bg-[url("./assets/images/bg-pattern-footer-desktop.svg")] bg-no-repeat bg-top lg:bg-top-left bg-size-[100%_auto] lg:bg-size-[35%]'>
+            <div className='lg:flex lg:items-center lg:justify-between'>
+                <img src={logo} alt="logo" className='mx-auto lg:mx-0' />
+                <div className='flex items-center justify-center text-gray-600 gap-2 mt-8 lg:mt-0 text-[28px]'>
+                    <FontAwesomeIcon icon={faSquareFacebook} className='hover:text-black cursor-pointer' />
+                    <FontAwesomeIcon icon={faTwitter} className='hover:text-black cursor-pointer' />
+                    <FontAwesomeIcon icon={faPinterest} className='hover:text-black cursor-pointer' />
+                    <FontAwesomeIcon icon={faInstagram} className='hover:text-black cursor-pointer' />
+                </div>
             </div>
             <hr className='text-gray-300 border my-10' />
-            <h1 className='text-gray-500 font-medium text-[15px]'>OUR COMPANY</h1>
-            <div className='my-8 space-y-1 font-semibold'>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>HOW WE WORK</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>WHY INSURE?</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>CHECK PRICE</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>REVIEWS</p>
-            </div>
-            <h1 className='text-gray-500 font-medium text-[15px]'>HELP ME</h1>
-            <div className='my-8 space-y-1 font-semibold'>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>FAQ</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>TERMS OF USE</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>PRIVACY POLICY</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>COOKIES</p>
-            </div>
-            <h1 className='text-gray-500 font-medium text-[15px]'>CONTACT</h1>
-            <div className='my-8 space-y-1 font-semibold'>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>SALES</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>SUPPORT</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>LIVE CHAT</p>
-            </div>
-            <h1 className='text-gray-500 font-medium text-[15px]'>OTHERS</h1>
-            <div className='mt-8 space-y-1 font-semibold'>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>CAREERS</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>PRESS</p>
-                <p className='hover:underline cursor-pointer w-fit mx-auto'>LICENSES</p>
+            <div className='lg:flex lg:gap-50'>
+                <div>
+                    <h1 className='text-gray-500 font-medium text-[15px]'>OUR COMPANY</h1>
+                    <div className='my-8 space-y-1 font-semibold'>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>HOW WE WORK</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>WHY INSURE?</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>CHECK PRICE</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>REVIEWS</p>
+                    </div>
+                </div>
+                <div>
+                    <h1 className='text-gray-500 font-medium text-[15px]'>HELP ME</h1>
+                    <div className='my-8 space-y-1 font-semibold'>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>FAQ</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>TERMS OF USE</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>PRIVACY POLICY</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>COOKIES</p>
+                    </div>
+                </div>
+                <div>
+                    <h1 className='text-gray-500 font-medium text-[15px]'>CONTACT</h1>
+                    <div className='my-8 space-y-1 font-semibold'>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>SALES</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>SUPPORT</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>LIVE CHAT</p>
+                    </div>
+                </div>
+                <div>
+                    <h1 className='text-gray-500 font-medium text-[15px]'>OTHERS</h1>
+                    <div className='mt-8 space-y-1 font-semibold'>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>CAREERS</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>PRESS</p>
+                        <p className='hover:underline cursor-pointer w-fit mx-auto lg:mx-0'>LICENSES</p>
+                    </div>
+                </div>
             </div>
         </footer>
     )
--- Frontendmentor/Insure-landing-page/src/components/NavBar.jsx
+++ Frontendmentor/Insure-landing-page/src/components/NavBar.jsx
@@ -12,9 +12,17 @@ function NavBar() {
       }, [open])
 
     return (
-        <nav className={`p-6 flex items-center justify-between ${open ? 'fixed top-0 left-0 w-full z-20 bg-white' : 'relative'}`}>
+        <nav className={`p-6 lg:px-30 flex items-center justify-between ${open ? 'fixed top-0 left-0 w-full z-20 bg-white' : 'relative'}`}>
             <img src={logo} alt="logo" />
-            <img src={open ? close : hamburger} alt="menu" className='cursor-pointer' onClick={() => setOpen(!open)} />
+            <div className='hidden lg:block'>
+                <div className='flex items-center gap-8 font-medium'>
+                    <p className='text-gray-400 cursor-pointer hover:text-black'>HOW WE WORK</p>
+                    <p className='text-gray-400 cursor-pointer hover:text-black'>BLOG</p>
+                    <p className='text-gray-400 cursor-pointer hover:text-black'>ACCOUNT</p>
+                    <button className='border-2 p-2 px-6 hover:bg-[#2B282F] hover:text-white cursor-pointer'>VIEW PLANS</button>
+                </div>
+            </div>
+            <img src={open ? close : hamburger} alt="menu" className='cursor-pointer block lg:hidden' onClick={() => setOpen(!open)} />
             { open && (
                 <div className='fixed text-white text-center text-[24px] space-y-8 py-18 tracking-[1px] h-screen w-screen left-0 z-11 top-20 p-8 bg-[#2B282F] bg-[url("./assets/images/bg-pattern-mobile-nav.svg")] bg-no-repeat bg-bottom bg-size-[100%_auto]'>
                     <p className='cursor-pointer hover:text-gray-300 w-fit mx-auto'>HOW WE WORK</p>
