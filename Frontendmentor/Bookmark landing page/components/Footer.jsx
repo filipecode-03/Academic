8de62817fd2cbc1617bf99bb6e24c147fa760ffa
Filipe Magalhães <contatofilipecode@gmail.com>
2026-02-25 16:58:16 -0400

    update

--- Frontendmentor/Bookmark landing page/components/Footer.jsx
+++ Frontendmentor/Bookmark landing page/components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
         <footer className="bg-[#242946] p-8 text-white">
             <img src={logoWhite} alt="logo" className='mx-auto' />
             <div className='flex flex-col text-center mt-8 gap-5 font-light tracking-[1px] text-[14px]'>
-                <a href="#">FEATURES</a>
-                <a href="#">PRICING</a>
-                <a href="#">CONTACT</a>
+                <a href="#" className='w-fit mx-auto'>FEATURES</a>
+                <a href="#" className='w-fit mx-auto'>PRICING</a>
+                <a href="#" className='w-fit mx-auto'>CONTACT</a>
             </div>
             <div className='flex justify-center gap-5 text-[25px] mt-8'>
                 <FontAwesomeIcon icon={faSquareFacebook} />
--- Frontendmentor/Bookmark landing page/components/Form.jsx
+++ Frontendmentor/Bookmark landing page/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import erro from '../src/assets/images/icon-error.svg'
 
 function Form() {
 
@@ -13,15 +14,29 @@ function Form() {
     }
 
     return (
-        <section className="bg-[#5266DF] mt-20 text-center p-8 py-16 text-white">
-            <p className="tracking-[5px] text-[12px]">35.000+ ALREADY JOINED</p>
-            <h1 className="text-[25px] font-medium w-[80%] mx-auto leading-8 mt-2">Stay up-to-date with what we-re doing</h1>
+        <section className="bg-[#5266DF] mt-20 p-8 py-16 text-white">
+            <p className="tracking-[5px] text-[12px] text-center">35.000+ ALREADY JOINED</p>
+            <h1 className="text-[25px] font-medium w-[80%] mx-auto text-center leading-8 mt-2">Stay up-to-date with what we-re doing</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col mt-6 text-[14px] gap-4">
-                <input type="email" className="bg-white text-gray-400 p-3 rounded-[5px] w-[80%] mx-auto" placeholder="Enter your email address"
-                {...register("email", { required: "Email é obrigatório" })} 
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-                <button type="submit" className="bg-[#FB5859] p-3 rounded-[5px] w-[80%] mx-auto">Contact Us</button>
+                <div className={`relative w-[80%] mx-auto rounded-[5px] p-1 transition-all ${errors.email ? "bg-[#FB5859]" : ""}`}>
+                    <input
+                        type="email"
+                        placeholder="Enter your email address"
+                        className="bg-white text-black placeholder:text-gray-400 p-3 rounded-[5px] w-full outline-none"
+                        {...register("email", {
+                        required: "Whoops, make sure it's an email"
+                        })}
+                    />
+                    {errors.email && (
+    <img
+      src={erro}
+      alt="error icon"
+      className="absolute right-5 top-6.5 -translate-y-1/2"
+    />
+  )}
+                    {errors.email && <span className="block mt-2 mb-0.5 pl-3 text-white text-[12px]">{errors.email.message}</span>}
+                </div>
+                <button type="submit" className="cursor-pointer bg-[#FB5859] p-3 rounded-[5px] w-[80%] mx-auto">Contact Us</button>
             </form>
         </section>
     )
