59969105f444cacea04a495f61d4eb06d8b50e6a
Filipe Magalhães <contatofilipecode@gmail.com>
2026-04-08 17:01:37 -0400

    update

--- Frontendmentor/crowdfunding product page/src/components/SectionTwo.jsx
+++ Frontendmentor/crowdfunding product page/src/components/SectionTwo.jsx
@@ -1,16 +1,16 @@
 function SectionTwo() {
     return (
-        <section className="bg-white text-center p-10 mt-8 shadow-2xs rounded-[10px]">
-            <div className="flex flex-col gap-8">
-                <div>
+        <section className="bg-white text-center lg:w-[55%] lg:mx-auto p-10 mt-8 shadow-2xs rounded-[10px]">
+            <div className="flex flex-col lg:flex-row gap-8">
+                <div className="border-r border-gray-400 pr">
                     <h2 className="font-bold text-[35px]">$89,914</h2>
                     <p className="text-gray-600">of $100,000 backed</p>
-                    <hr className="w-[26%] text-gray-400 mt-5 mx-auto" />
+                    <hr className="w-[26%] block lg:hidden text-gray-400 mt-5 mx-auto" />
                 </div>
                 <div>
                     <h2 className="font-bold text-[35px]">5,007</h2>
                     <p className="text-gray-600">total backers</p>
-                    <hr className="w-[26%] text-gray-400 mt-5 mx-auto" />
+                    <hr className="w-[26%] block lg:hidden text-gray-400 mt-5 mx-auto" />
                 </div>
                 <div>
                     <h2 className="font-bold text-[35px]">56</h2>
