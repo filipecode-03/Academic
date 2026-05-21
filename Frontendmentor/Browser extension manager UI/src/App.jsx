0ec4eed1d5196fb2f9760ddd9b83466554a8c1f8
Filipe Magalhães <contatofilipecode@gmail.com>
2026-05-21 10:53:58 -0400

    finished the project

--- Frontendmentor/Browser extension manager UI/src/App.jsx
+++ Frontendmentor/Browser extension manager UI/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, [theme]);
 
   return (
-    <div className="bg-neutral-100 p-6 dark:bg-neutral-900 transition-colors duration-300">
+    <div className="bg-neutral-100 min-h-screen font-['Noto_Sans'] p-6 dark:bg-linear-to-b dark:from-[#050A1E] dark:to-[#0A1640] transition-colors duration-300">
       <Nav theme={theme} toggleTheme={toggleTheme} />
       <ExtensionsList />
     </div>
--- Frontendmentor/Browser extension manager UI/src/components/Nav.jsx
+++ Frontendmentor/Browser extension manager UI/src/components/Nav.jsx
@@ -1,15 +1,15 @@
 import { Moon, Sun } from "lucide-react";
 import logo from '../images/logo.svg'
+import logoDark from '../images/logo-dark.svg'
 import moon from '../images/icon-moon.svg'
 import sun from '../images/icon-sun.svg'
 
 function Nav({ theme, toggleTheme }) {
   return (
-    <header className="w-full">
-      <nav className="mx-auto flex items-center justify-between bg-white dark:bg-neutral-800 px-6 py-4 rounded-2xl shadow-md">
-        <img src={logo} alt="logo" />
-        <button onClick={toggleTheme}
-          className="p-3 rounded-xl cursor-pointer bg-[#EEEEEE] dark:bg-neutral-700 hover:bg-neutral-300 dark:hover:bg-neutral-600 transition-all duration-300">
+    <header className="w-full lg:max-w-280 lg:mx-auto">
+      <nav className="mx-auto flex items-center justify-between bg-white dark:bg-neutral-800 p-4 rounded-2xl shadow-md">
+        <img src={theme === "dark" ? logoDark : logo} alt="logo"/>        
+        <button onClick={toggleTheme} className="p-3 rounded-xl cursor-pointer bg-[#EEEEEE] dark:bg-neutral-700 hover:bg-[#C6C6C4] dark:hover:bg-neutral-600 focus:outline-none focus:ring-2 focus:ring-[#C6231D] focus:ring-offset-2 dark:focus:ring-offset-[#050A1E] transition-all duration-300">          
           {theme === "light" ? (
             <img src={moon} alt="moon" />
           ) : (
