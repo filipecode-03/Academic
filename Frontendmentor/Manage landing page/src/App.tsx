74e391fd6ab8161b1a88aca1b8185dfe84ee2024
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-06-16 17:05:38 -0400

    update

--- Frontendmentor/Manage landing page/src/App.tsx
+++ Frontendmentor/Manage landing page/src/App.tsx
@@ -1,13 +1,16 @@
 import Footer from "./components/Footer"
 import Started from "./components/Started"
+import Nav from './components/Nav'
 import bg from './assets/images/bg-tablet-pattern.svg'
 
 function App() {
 
   return (
     <div className="relative font-['Be_Vietnam_Pro'] min-h-screen">
-      <img src={bg} alt="bg" className="absolute -top-20 -right-20" />
-      
+      <img src={bg} alt="bg" className="absolute -z-1 -top-20 -right-20" />
+      <div className="p-8">
+        <Nav />
+      </div>
       <Started />
       <Footer />
     </div>
--- Frontendmentor/Manage landing page/src/components/Nav.tsx
+++ Frontendmentor/Manage landing page/src/components/Nav.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react"
+import logo from '../assets/images/logo.svg'
+import { Menu, X } from 'lucide-react';
+import { useFloating, offset, flip, shift, autoUpdate } from "@floating-ui/react";
+
 function Nav() {
+
+    const [open, setOpen] = useState(false)
+    
+    const { x, y, refs, strategy } = useFloating({
+    placement: "bottom-start",
+    middleware: [offset(8), flip(), shift({ padding: 8 })],
+    whileElementsMounted: autoUpdate,
+  });
+
     return (
         <nav>
-
+            <div className="flex items-center justify-between">
+                <img src={logo} alt="logo" />
+                <Menu ref={refs.setReference} onClick={() => setOpen(true)} />
+                {open && (
+                    <div ref={refs.setFloating}
+                        style={{
+                            position: strategy,
+                            top: y ?? 0,
+                            left: x ?? 0,
+                        }}
+                        className="bg-white text-center shadow-lg p-4 rounded w-113"
+                    >
+                        <ul className="space-y-5 font-semibold">
+                            <li>Pricing</li>
+                            <li>Product</li>
+                            <li>About Us</li>
+                            <li>Careers</li>
+                            <li>Community</li>
+                        </ul>
+                    </div>
+                )}
+            </div>
         </nav>
     )
 }
