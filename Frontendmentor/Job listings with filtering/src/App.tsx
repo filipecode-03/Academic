cbb14a729daf004558b30df281d58142c158c7ad
Filipe Magalhães <contatofilipecode@gmail.com>
2026-06-20 11:50:35 -0400

    inicio das modificaçõs do gpt

--- Frontendmentor/Job listings with filtering/src/App.tsx
+++ Frontendmentor/Job listings with filtering/src/App.tsx
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <div>
-
+    <div className="font-['League_Spartan']">
+      <Header />
     </div>
   )
 }
---
+++ Frontendmentor/Job listings with filtering/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header
+      className="header-bg h-40"
+    />
+  );
+}
+
+export default Header;
