e11e53c30ad549ffb5bc83687d4f36a556effc6c
Filipe Magalhães <contatofilipecode@gmail.com>
2026-06-19 22:48:40 -0400

    Update Nav.tsx

--- Frontendmentor/Manage landing page/src/components/Nav.tsx
+++ Frontendmentor/Manage landing page/src/components/Nav.tsx
@@ -1,70 +1,126 @@
-import { useState } from "react"
-import logo from '../assets/images/logo.svg'
-import { Menu, X } from 'lucide-react';
-import { useFloating, offset, flip, shift, autoUpdate } from "@floating-ui/react";
+import { useState } from "react";
+import logo from "../assets/images/logo.svg";
+import { Menu, X } from "lucide-react";
+import {
+  useFloating,
+  offset,
+  flip,
+  shift,
+  autoUpdate,
+} from "@floating-ui/react";
 
 function Nav() {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false)
-    
-    const { x, y, refs, strategy } = useFloating({
-    placement: "bottom-start",
-    middleware: [offset(8), flip(), shift({ padding: 8 })],
+  const { x, y, refs, strategy } = useFloating({
+    placement: "bottom-end",
+    middleware: [offset(16), flip(), shift({ padding: 16 })],
     whileElementsMounted: autoUpdate,
   });
 
-    const Icon = open ? X : Menu;
+  const Icon = open ? X : Menu;
 
-    return (
-        <nav className="p-8">
-            {open && (
-            <div
-                className="fixed inset-0 bg-linear-to-b from-black/0 to-black/70 z-10"
+  return (
+    <nav className="px-8 py-8 lg:px-0">
+      <div className="mx-auto flex max-w-[1120px] items-center justify-between">
+        {/* Logo */}
+        <img src={logo} alt="Manage Logo" />
+        {/* Desktop */}
+        <div className="hidden lg:flex items-center gap-10">
+          <ul className="flex items-center gap-8 text-[14px] font-medium">
+            <li className="cursor-pointer hover:text-gray-400 transition-colors">
+              Pricing
+            </li>
+            <li className="cursor-pointer hover:text-gray-400 transition-colors">
+              Product
+            </li>
+            <li className="cursor-pointer hover:text-gray-400 transition-colors">
+              About Us
+            </li>
+            <li className="cursor-pointer hover:text-gray-400 transition-colors">
+              Careers
+            </li>
+            <li className="cursor-pointer hover:text-gray-400 transition-colors">
+              Community
+            </li>
+          </ul>
+          <button
+            className="
+              cursor-pointer
+              rounded-full
+              bg-[#F3613C]
+              px-8
+              py-3
+              text-white
+              font-semibold
+              shadow-lg
+              transition
+              hover:opacity-80
+            "
+          >
+            Get Started
+          </button>
+        </div>
+        {/* Mobile */}
+        <div className="lg:hidden">
+          <Icon
+            ref={refs.setReference}
+            onClick={() => setOpen((prev) => !prev)}
+            className="cursor-pointer"
+          />
+
+          {open && (
+            <>
+              <div
+                className="fixed inset-0 z-10 bg-gradient-to-b from-transparent to-black/70"
                 onClick={() => setOpen(false)}
-            />
-            )}
+              />
 
-            <div className="flex items-center justify-between relative z-30">
-            <img src={logo} alt="logo" />
-
-            <Icon
-                ref={refs.setReference}
-                onClick={() => setOpen((prev) => !prev)}
-                className="cursor-pointer"
-            />
-
-            {open && (
-                <div
+              <div
                 ref={refs.setFloating}
                 style={{
-                    position: strategy,
-                    top: y ?? 0,
-                    left: x ?? 0,
+                  position: strategy,
+                  top: y ?? 0,
+                  left: x ?? 0,
                 }}
-                className="bg-white text-center shadow-lg p-4 rounded w-md z-20"
-                >
+                className="
+                  z-20
+                  w-[320px]
+                  rounded-md
+                  bg-white
+                  p-6
+                  text-center
+                  shadow-xl
+                "
+              >
                 <ul className="space-y-5 font-semibold">
-                    <li className="hover:text-[#F2613C] w-fit mx-auto cursor-pointer">
+                  <li className="cursor-pointer hover:text-[#F3613C]">
                     Pricing
-                    </li>
-                    <li className="hover:text-[#F2613C] w-fit mx-auto cursor-pointer">
+                  </li>
+
+                  <li className="cursor-pointer hover:text-[#F3613C]">
                     Product
-                    </li>
-                    <li className="hover:text-[#F2613C] w-fit mx-auto cursor-pointer">
+                  </li>
+
+                  <li className="cursor-pointer hover:text-[#F3613C]">
                     About Us
-                    </li>
-                    <li className="hover:text-[#F2613C] w-fit mx-auto cursor-pointer">
+                  </li>
+
+                  <li className="cursor-pointer hover:text-[#F3613C]">
                     Careers
-                    </li>
-                    <li className="hover:text-[#F2613C] w-fit mx-auto cursor-pointer">
+                  </li>
+
+                  <li className="cursor-pointer hover:text-[#F3613C]">
                     Community
-                    </li>
+                  </li>
                 </ul>
-                </div>
-            )}
-            </div>
-        </nav>
-    );
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
