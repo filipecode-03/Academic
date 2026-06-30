b526ef7d95ff5af86f6cd050c74c29ffd877dc58
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-06-30 17:04:24 -0400

    update

--- Frontendmentor/Spc Acolhida/src/components/Inicio/Métricas.tsx
+++ Frontendmentor/Spc Acolhida/src/components/Inicio/Métricas.tsx
@@ -1,10 +1,10 @@
-import { CalendarDays, ListChecks, ClipboardPaste } from "lucide-react"
+import { CalendarDays, ListChecks, ClipboardPaste, Boxes, Wifi, Wrench, Map } from "lucide-react"
 
 function Metricas() {
     return (
-        <div className="bg-[#003049] p-8">
-            <h1 className="text-3xl lg:text-4xl text-white font-bold text-center">Métrica dos Processo Realizados em 2026</h1>
-            <div className="text-white mt-6 flex flex-col gap-4">
+        <div className="bg-[#003049] text-white p-8">
+            <h1 className="text-3xl lg:text-4xl font-bold text-center">Métrica dos Processo Realizados em 2026</h1>
+            <div className=" mt-6 flex flex-col gap-4">
                 <div>
                     <CalendarDays />
                     <h2>Demanda Mensal</h2>
@@ -22,7 +22,43 @@ function Metricas() {
                 </div>
             </div>
             <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6yuyRYyY2J8p2KhFfrrPYpCmPsHqiiBocQ_NLth36DB42eErcDVcfhEqRkJZKMw/pubchart?oid=105442287&format=interactive">link</a>
-            
+            <div>
+                <div>
+                    <h2>Consumo (33.90.30)</h2>
+                    <p>Os materiais e serviços classificados como consumo são aqueles que possuem duração inferior a dois anos, sendo utilizados de forma contínua ou que se desgastam rapidamente. Por isso, são registrados na natureza de despesa 33.90.30, destinada a itens de uso imediato ou rotineiro.</p>
+                </div>
+                <div>
+                    <h2>Permanente (44.90.52)</h2>
+                    <p>Os materiais e serviços considerados permanentes possuem vida útil superior a dois anos, conservando suas características e funcionalidade por longos períodos. Esses itens são registrados na natureza de despesa 44.90.52, voltada para bens duráveis e de caráter estrutural.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h2 className="text-center">Principais demandantes</h2>
+                    <div>
+                        <div>
+                            <Boxes />
+                            <p>Almox - Almoxarifado</p>
+                        </div>
+                        <div>
+                            <Wifi />
+                            <p>D6 - Seção de Comunicações e Sistemas de Informação</p>
+                        </div>
+                        <div>
+                            <Wrench />
+                            <p>COE - Centro de Operações de Engenharia</p>
+                        </div>
+                        <div>
+                            <Map />
+                            <p>D4 - Centro de Coordenações de Operaçãoes Logísticas</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="text-center">Integrar & Acolher</h2>
+                    <p>Cada setor da Operação Acolhida desempenha funções cruciais e, por isso, demanda continuamente materiais, equipamentos e serviços para garantir a continuidade das operações.</p>
+                </div>
+            </div>
         </div>
     )
 }
