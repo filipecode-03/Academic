d9a71d53a634b4923d892e98c99aa7fa589d4a3b
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-07-06 13:58:00 -0400

    update

---
+++ Frontendmentor/my-app/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import Logo from '@/public/images/logo.svg'
+
+function Footer() {
+    return (
+        <footer className="bg-[#232027]">
+            <Logo className="dark:invert" src="/logo.svg" alt="Next.js logo" />
+        </footer>
+    )
+}
+
+export default Footer
---
+++ Frontendmentor/my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "./components/footer";
+
+
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen text-black">
+      <h1>fibvcdfib</h1>
+      <Footer />
+
+    </div>
+  );
+}
