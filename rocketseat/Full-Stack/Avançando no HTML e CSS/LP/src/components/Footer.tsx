46033054b79921dc893c059cca9578fa3af849b9
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-07-21 15:08:38 -0400

    Update Footer.tsx

--- rocketseat/Full-Stack/Avançando no HTML e CSS/LP/src/components/Footer.tsx
+++ rocketseat/Full-Stack/Avançando no HTML e CSS/LP/src/components/Footer.tsx
@@ -1,7 +1,41 @@
+import logo from '../assets/images/Logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXTwitter, faInstagram, faDiscord, faTiktok } from '@fortawesome/free-brands-svg-icons'
+
 function Footer() {
     return (
-        <footer>
-
+        <footer className="p-12">
+            <div className='flex font-medium flex-col gap-12'>
+                <img src={logo} alt="logo" className='w-21.25' />
+                <div className='flex flex-col gap-16'>
+                    <div className='flex text-[#A1A1AA] flex-col gap-5'>
+                        <h2 className='text-[14px] text-[#F7B733]'>PRODUTO</h2>
+                        <a href="#">Funcionalidades</a>
+                        <a href="#">Planos e preços</a>
+                        <a href="#">Download</a>
+                    </div>
+                    <div className='flex text-[#A1A1AA] flex-col gap-5'>
+                        <h2 className='text-[14px] text-[#F7B733]'>EMPRESA</h2>
+                        <a href="#">Sobre nós</a>
+                        <a href="#">Trabalhe conosco</a>
+                        <a href="#">Blog</a>
+                    </div>
+                    <div className='flex text-[#A1A1AA] flex-col gap-5'>
+                        <h2 className='text-[14px] text-[#F7B733]'>LEGAL</h2>
+                        <a href="#">Termos de uso</a>
+                        <a href="#">Política de privacidade</a>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-12 flex flex-col gap-6 text-[#A1A1AA]'>
+                <div className='text-[24px] flex items-center gap-6'>
+                    <FontAwesomeIcon icon={faTiktok} />
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <FontAwesomeIcon icon={faXTwitter} />
+                    <FontAwesomeIcon icon={faDiscord} />
+                </div>
+                <p>©2024 Zingen Technologies, Inc.</p>
+            </div>
         </footer>
     )
 }
