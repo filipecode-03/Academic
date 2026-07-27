7174e1ff4ab52a74374f2f2af3625bb24e0b30d6
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-07-27 11:11:51 -0400

    teste de animação do kit

--- rocketseat/Full-Stack/Avançando no HTML e CSS/Clube/src/components/Kit.tsx
+++ rocketseat/Full-Stack/Avançando no HTML e CSS/Clube/src/components/Kit.tsx
@@ -1,7 +1,12 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import book from '../assets/images/book.png'
-import folheto01 from '../assets/images/'
+import folheto01 from '../assets/images/bookmark-yellow.png'
+import folheto02 from '../assets/images/bookmark-purple.png'
+import pin from '../assets/images/pin.png'
+import arrowFolheto from '../assets/images/arrowFolheto.png'
+import arrowBook from '../assets/images/arrowBook.png'
+import arrowPin from '../assets/images/arrowPin.png'
 
 function Kit() {
     return (
@@ -13,7 +18,20 @@ function Kit() {
             </div>
             <h3 className="text-center">Recebe uma caixa surpresa todos os meses</h3>
             <div>
-
+                <img src={book} alt="book" />
+                <div>
+                    <p>Livro em capa dura</p>
+                    <img src={arrowBook} alt="arrowBook" />
+                </div>
+                <img src={folheto01} alt="folheto01" />
+                <img src={folheto02} alt="folheto02" />
+                <div>
+                    <img src={arrowFolheto} alt="arrowFolheto" />
+                    <p>Marca páginas temático</p>
+                </div>
+                <p>Broche exclusivo</p>
+                <img src={arrowPin} alt="arrowPin" />
+                <img src={pin} alt="pin" />
             </div>
         </section>
     )
