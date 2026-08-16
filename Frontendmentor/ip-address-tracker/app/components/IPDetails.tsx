5e3445bfbdb68c0684792eb007a816a6827666c1
Filipe Magalhães <contatofilipecode@gmail.com>
2026-08-16 11:45:56 -0400

    Update IPDetails.tsx

--- Frontendmentor/ip-address-tracker/app/components/IPDetails.tsx
+++ Frontendmentor/ip-address-tracker/app/components/IPDetails.tsx
@@ -10,7 +10,7 @@ function IPDetails({ data }: IPDetailsProps) {
   }
 
   return (
-    <section className="absolute top-55 left-1/2 z-[1100] w-[90%] max-w-275 -translate-x-1/2 rounded-[15px] bg-white px-6 py-7 shadow-lg">
+    <section className="absolute top-55 left-1/2 z-1100 w-[90%] max-w-275 -translate-x-1/2 rounded-[15px] bg-white px-6 py-7 shadow-lg">
       <div className="grid grid-cols-1 gap-6 text-center md:grid-cols-4 md:gap-0 md:text-left">
         <div className="flex flex-col gap-2 md:px-8">
           <span className="text-[10px] font-bold tracking-[2px] text-gray-500">
