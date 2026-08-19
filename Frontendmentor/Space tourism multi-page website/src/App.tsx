10e8d13cb5bd60ec6349eaae69035e24af353969
filipe magalhaes de oliveira cruz <contatofilipecode@gmail.com>
2026-08-19 16:50:26 -0400

    update

--- Frontendmentor/Space tourism multi-page website/src/App.tsx
+++ Frontendmentor/Space tourism multi-page website/src/App.tsx
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 
 function App() {
-
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/destination" element={<Destination />} />
+
+        <Route path="/crew" element={<Crew />} />
+
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
